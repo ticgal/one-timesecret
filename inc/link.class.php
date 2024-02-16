4d7b63652399827146f39e7a20e849f25c651a50
FrancisTicgal <francisco@ticgal.com>
2024-02-16 12:19:19 +0100

    update headers

--- inc/link.class.php
+++ inc/link.class.php
@@ -1,34 +1,34 @@
 <?php
 
-/*
--------------------------------------------------------------------------
-OneTimeSecret plugin for GLPI
-Copyright (C) 2021-2023 by the TICgal Team.
-https://www.tic.gal
--------------------------------------------------------------------------
-LICENSE
-This file is part of the OneTimeSecret plugin.
-OneTimeSecret plugin is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-OneTimeSecret plugin is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with OneTimeSecret. If not, see
-<http: //www.gnu.org/licenses />.
---------------------------------------------------------------------------
-@package OneTimeSecret
-@author the TICgal team
-@copyright Copyright (c) 2021-2023 TICgal team
-@license AGPL License 3.0 or (at your option) any later version
-http://www.gnu.org/licenses/agpl-3.0-standalone.html
-@link https://www.tic.gal
-@since 2021-2023
-----------------------------------------------------------------------
-*/
+/**
+ * -------------------------------------------------------------------------
+ * OneTimeSecret plugin for GLPI
+ * Copyright (C) 2021-2024 by the TICgal Team.
+ * https://www.tic.gal
+ * -------------------------------------------------------------------------
+ * LICENSE
+ * This file is part of the OneTimeSecret plugin.
+ * OneTimeSecret plugin is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * OneTimeSecret plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with OneTimeSecret. If not, see
+ * <http: //www.gnu.org/licenses />.
+ * -------------------------------------------------------------------------
+ * @package OneTimeSecret
+ * @author the TICgal team
+ * @copyright Copyright (c) 2021-2024 TICgal team
+ * @license AGPL License 3.0 or (at your option) any later version
+ * http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link https://www.tic.gal
+ * @since 2021
+ * -------------------------------------------------------------------------
+ */
 
 if (!defined('GLPI_ROOT')) {
     die("Sorry. You can't access directly to this file");
