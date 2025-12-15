aee16fe35cfddd5ad3a6fef186615dae8d206f4f
Pedro Rocha <jpnr79@gmail.com>
2025-12-15 00:05:49 +0000

    fix: ensure GLPI_ROOT is defined in multiple files for consistency

--- inc/link.class.php
+++ inc/link.class.php
@@ -1,5 +1,5 @@
 <?php
-
+/*if (!defined('GLPI_ROOT')) { define('GLPI_ROOT', realpath(__DIR__ . '/../..')); }
 /*
 -------------------------------------------------------------------------
 OneTimeSecret plugin for GLPI
